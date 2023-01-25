67df0d648e90f4646da9778234bf2d54b40822e4
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-01-25 20:02:43 +0100

    p6v0.1.0

--- src/scripts/factories/photographer.js
+++ src/scripts/factories/photographer.js
@@ -1,6 +1,6 @@
 module.exports = {
   create (data) {
-    const { name, portrait } = data
+    const { name, city, portrait, tagline, price } = data
 
     const picture = `/src/assets/photographers/${portrait}`
 
@@ -10,11 +10,20 @@ module.exports = {
       img.setAttribute('src', picture)
       const h2 = document.createElement('h2')
       h2.textContent = name
+      const h3 = document.createElement('h3')
+      h3.textContent = city
+      const p = document.createElement('p')
+      p.textContent = tagline
+      const prix = document.createElement('p')
+      prix.textContent = price
       article.appendChild(img)
       article.appendChild(h2)
+      article.appendChild(h3)
+      article.appendChild(p)
+      article.appendChild(prix)
       return (article)
     }
 
-    return { name, picture, getUserCardDOM }
+    return { name, city, picture, tagline, price, getUserCardDOM }
   }
 }
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,29 +1,13 @@
 const factoryPhotographer = require('../factories/photographer')
 
 const getPhotographers = async () => {
-  // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      name: 'Ma data test',
-      id: 1,
-      city: 'Paris',
-      country: 'France',
-      tagline: 'Ceci est ma data test',
-      price: 400,
-      portrait: 'account.png'
-    },
-    {
-      name: 'Autre data test',
-      id: 2,
-      city: 'Londres',
-      country: 'UK',
-      tagline: 'Ceci est ma data test 2',
-      price: 500,
-      portrait: 'account.png'
-    }
-  ]
-  // et bien retourner le tableau photographers seulement une fois
-  return ({ photographers: [...photographers, ...photographers, ...photographers] })
+  const photographers =
+    await
+    fetch('/src/data/photographers.json')
+      .then(res => res.json(res))
+      .then(data => console.log(data))
+  console.log(photographers)
+  return ({ photographers: [...photographers.photographers] })
 }
 
 const displayData = async photographers => {
