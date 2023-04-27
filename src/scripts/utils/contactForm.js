605ce2b9027b25a7bc276fc1a6ab16924f3106e0
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-04-28 00:27:47 +0200

    Correctif regex + FormModal

--- src/scripts/utils/contactForm.js
+++ src/scripts/utils/contactForm.js
@@ -1,10 +1,9 @@
-const { main, contactModal, contactButton, closeModal, formContact, prenom, nom, email, message } = require('./domLinker')
+const { main, contactModal, contactButton, closeModal, formContact, first, last, email, message } = require('./domLinker')
 
 // creation des evenements
 
 contactButton.addEventListener('click', () => displayModal())
 closeModal.addEventListener('click', () => closeForm())
-formContact.addEventListener('submit', e => formResult(e))
 
 // ouvrir la modal
 
@@ -27,16 +26,71 @@ const closeForm = () => {
   contactModal.style.display = 'none'
   contactModal.setAttribute('aria-hidden', true)
   main.setAttribute('aria-hidden', false)
+  first.value = ''
+  last.value = ''
+  email.value = ''
+  message.value = ''
 }
 
-const formResult = e => {
-  e.preventDefault()
-  console.log(`
-    firstname:${prenom.value}
-    lastname:${nom.value}
+const displayErrorMessage = (input, isValid) => {
+  if (isValid) {
+    input.parentNode.setAttribute('data-error-visible', false)
+  } else {
+    input.parentNode.setAttribute('data-error-visible', true)
+  }
+}
+const checkInput = (input, isValid) => {
+  displayErrorMessage(input, isValid)
+  return isValid
+}
+
+/**
+ * Creation des regex pour chaque formulaire via la fonction IsValid + Checkinput de la valeur
+ * @param {Boolean} = () => CheckInput, /Regex/.test(IdForm.value)}
+ */
+const isValidFirstname = () => checkInput(first, /([a-zA-Z_]){2,20}/.test(first.value))
+const isValidLastname = () => checkInput(last, /([a-zA-Z_]){2,20}/.test(last.value))
+const isValidEmail = () => checkInput(email, /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value))
+
+// ecoute de la valeur 'input' de l'Id first envoyé dans IsValid...
+
+first.addEventListener('input', isValidFirstname)
+
+// ecoute de la valeur 'input' de l'Id last envoyé dans IsValid...
+
+last.addEventListener('input', isValidLastname)
+
+// ecoute de la valeur 'input' de l'Id email envoyé dans IsValid...
+
+email.addEventListener('input', isValidEmail)
+
+// ecoute de la valeur 'input' de l'Id birthdate envoyé dans IsValid...
+
+/* Validation de tout les champ du Form Via la const inputsAreValid afin de recuperer une seul reponse de toute les champ */
+const inputsAreValid = () =>
+  isValidFirstname() && isValidLastname() && isValidEmail()
+
+/**
+* Change l'effet du boutton Submit
+* @param {Object} event
+ */
+const formResult = (event) => {
+  // rend le button non utilisable
+  event.preventDefault()
+  // si InputsAreValid alors return success
+  if (inputsAreValid()) {
+    console.log(`
+    firstname:${first.value}
+    lastname:${last.value}
     email:${email.value}
     message:${message.value}`)
+
+    closeForm()
+  }
 }
+
+// event submit form event
+formContact.addEventListener('submit', formResult)
 
 module.exports = {
   displayModal,
